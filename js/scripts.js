4973e28aa86f151c222b4eb6d93a63fa26d71c64
Felicia Liew <itsfelicialiew@gmail.com>
2021-01-22 22:38:49 +0800

    Update layout

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,3 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
-$('.anchor-scroll').anchorScroll({
-   scrollSpeed: 1500, // scroll speed
-   offsetTop: 0, // offset for fixed top bars (defaults to 0)
-   onScroll: function () {
-     // callback on scroll start
-   },
-   scrollEnd: function () {
-     // callback on scroll end
-   }
-});
